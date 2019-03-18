91fd02164a2565ff1ebb0f34496d1d39a4ecc730
MelodieUZH <melodie.hadzic@uzh.ch>
2019-03-18 20:11:55 +0100

    Update action display in "Transition analysis" when switching data type

--- MASH-FRET/MASH.m
+++ MASH-FRET/MASH.m
@@ -4019,8 +4019,14 @@ function popupmenu_thm_tpe_Callback(obj, evd, h)
 p = h.param.thm;
 if ~isempty(p.proj)
     proj = p.curr_proj;
-    if get(obj, 'Value') ~= p.curr_tpe(proj)
-        p.curr_tpe(proj) = get(obj, 'Value');
+    val = get(obj, 'Value');
+    if val ~= p.curr_tpe(proj)
+        
+        tpe_str = get(obj,'String');
+        setContPan(cat(2,'Select data: ',tpe_str{val}),'success', ...
+            h.figure_MASH);
+        
+        p.curr_tpe(proj) = val;
         h.param.thm = p;
         guidata(h.figure_MASH, h);
 
@@ -4028,13 +4034,6 @@ if ~isempty(p.proj)
         cla(h.axes_hist2);
 
         updateFields(h.figure_MASH, 'thm');
-
-        p = h.param.thm;
-        proj = p.curr_proj;
-        tpe = p.curr_tpe(proj);
-        tpe_str = get(obj,'String');
-        setContPan(cat(2,'Select data: ',tpe_str{tpe}),'success', ...
-            h.figure_MASH);
     end
 end
 
