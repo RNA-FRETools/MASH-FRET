ed6b4a01250a1223cd3234351db8c3baf49942be
MelodieUZH <melodie.hadzic@uzh.ch>
2019-04-20 23:25:16 +0200

    Fix error in checkBgPattern.m
    - fix error occurring when using the "Pattern" background
    - correct intensity units conversion
    - handle common user mistake by managing background images with dimensions different than for the simulated video

--- MASH-FRET/source/mod_simulation/checkBgPattern.m
+++ MASH-FRET/source/mod_simulation/checkBgPattern.m
@@ -1,5 +1,10 @@
 function [ok, p] = checkBgPattern(p, h_fig)
 
+% Last update: 20.4.2019 by Mélodie Hadzic
+% >> correct intensity units conversion
+% >> control dimensions of background image and manage communication with
+%    user.
+
 ok = 1;
 
 if ~isfield(p, 'bgImg') || isempty(p.bgImg)
@@ -8,8 +13,21 @@ if ~isfield(p, 'bgImg') || isempty(p.bgImg)
         ok = 0;
         return;
     end
-    if strcmp(intUnits, 'electron')
-        data.frameCur = arb2phtn(data.frameCur);
+    if strcmp(p.intUnits, 'electron')
+        [mu_y_dark,K,eta] = getCamParam(p.noiseType,p.camNoise);
+        data.frameCur = ele2phtn(data.frameCur,K,eta);
+        if size(data.frameCur,3)>1
+            data.frameCur = sum(data.frameCur,3);
+        end
+        if size(data.frameCur,1)~=p.movDim(1) || ...
+                size(data.frameCur,2)~=p.movDim(2)
+            setContPan(cat(2,'Dimensions of the background image are not ',...
+                'consistent with video dimensions: please adjust video ',...
+                'dimensions or modify the dimensions of the background ',...
+                'image.'),'error',h_fig);
+            ok = 0;
+            return;
+        end
     end
     p.bgImg = data;
 else
@@ -24,8 +42,21 @@ else
             ok = 0;
             return;
         end
-        if strcmp(intUnits, 'electron')
-            data.frameCur = arb2phtn(data.frameCur);
+        if strcmp(p.intUnits, 'electron')
+            [mu_y_dark,K,eta] = getCamParam(p.noiseType,p.camNoise);
+            data.frameCur = ele2phtn(data.frameCur,K,eta);
+            if size(data.frameCur,3)>1
+                data.frameCur = sum(data.frameCur,3);
+            end
+            if size(data.frameCur,1)~=p.movDim(1) || ...
+                    size(data.frameCur,2)~=p.movDim(2)
+                setContPan(cat(2,'Dimensions of the background image are ',...
+                    'not consistent with video dimensions: please adjust ',...
+                    'video dimensions or modify the dimensions of the ',...
+                    'background image.'),'error',h_fig);
+                ok = 0;
+                return;
+            end
         end
         p.bgImg = data;
     end
