512990816443ef9342a16b4985204de0a6b005ad
MelodieUZH <melodie.hadzic@uzh.ch>
2019-04-25 10:30:48 +0200

    Revert "Correct double update of molecule list"
    This reverts commit 0a3bcb8b0783b9297ee8eb8673633d7ca223ac4c.

--- MASH-FRET/source/mod-trace-processing/_callbacks/pushbutton_molPrev_Callback.m
+++ MASH-FRET/source/mod-trace-processing/_callbacks/pushbutton_molPrev_Callback.m
@@ -1,9 +1,4 @@
 function pushbutton_molPrev_Callback(obj, evd, h)
-
-%% Last update by MH, 24.4.2019
-% >> remove double update of molecule list
-%%
-
 p = h.param.ttPr;
 if ~isempty(p.proj)
     proj = p.curr_proj;
@@ -21,10 +16,8 @@ if ~isempty(p.proj)
         end
         h.param.ttPr = p;
         guidata(h.figure_MASH, h);
-        
-        % cancelled by MH, 24.4.2019
-%         ud_trSetTbl(h.figure_MASH);
 
+        ud_trSetTbl(h.figure_MASH);
         updateFields(h.figure_MASH, 'ttPr');
     end
 end
