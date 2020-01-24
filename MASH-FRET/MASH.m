1fc76467d44262f24acab577e114b7c1a6d09d31
mca-sh <melodie.hadzic@protonmail.com>
2020-01-24 11:42:22 +0100

    unform code appearance

--- MASH-FRET/MASH.m
+++ MASH-FRET/MASH.m
@@ -13,22 +13,24 @@ for i = 1:size(mtlbDat,2)
 end
 
 % get MASH-FRET version
-%----------------
 % version_number = 'x.x.x'; % Versioning without folder structure %2018-03-07
 % versioning based on latest git tag and current commit hash; FS, 27.3.2019
-release_version_file = fullfile(codePath, '.release_version.json');
-if exist(release_version_file, 'file') == 2
+release_vers_file = fullfile(codePath, '.release_version.json');
+if exist(release_vers_file, 'file') == 2
     if str2num(mtlbDat(1,i).Version) >= 9.1  % use json loader introduced in Matlab 2016b
-        git_tag_commit_hash = jsondecode(fileread(release_version_file));
+        git_tag_commit_hash = jsondecode(fileread(release_vers_file));
     else
         % parse json file with regex
-        git_tag_commit_hash = regexp(regexprep(fileread(release_version_file), '\n+', ' '), '{(?:.|\n)*\"\<tag\>\"\s*\:\s*\"(?<tag>\d+\.\d+\.\d+)\"(?:.|\n)*\"\<prev_commit_hash\>\"\s*\:\s*\"(?<prev_commit_hash>\w*)\"(?:.|\n)*}', 'names');
+        git_tag_commit_hash = regexp(regexprep(fileread(release_vers_file),...
+            '\n+', ' '), cat(2,'{(?:.|\n)*\"\<tag\>\"\s*\:\s*\"(?<tag>\d+',...
+            '\.\d+\.\d+)\"(?:.|\n)*\"\<prev_commit_hash\>\"\s*\:\s*\"(?',...
+            '<prev_commit_hash>\w*)\"(?:.|\n)*}'), 'names');
     end
-    version_str = sprintf('%s (prev. commit: %s)', git_tag_commit_hash.tag, git_tag_commit_hash.prev_commit_hash);
+    version_str = sprintf('%s (prev. commit: %s)', git_tag_commit_hash.tag,...
+        git_tag_commit_hash.prev_commit_hash);
 else
     version_str = '(unknown version)';
 end
-%----------------
 
 % define figure name from MASH-FRET version
 figName = sprintf('%s %s','MASH-FRET', version_str);
@@ -38,18 +40,6 @@ if str2num(mtlbDat(1,i).Version) < 7.12
         figName ', i.e. 7.12. Be aware that compatibility problems can ',...
         'occur.']);
 end
-% % define figure name from folder name
-% [pname,o,o] = fileparts(which('MASH'));
-% issep = true;
-% while issep
-%     possep = strfind(pname,filesep);
-%     if isempty(possep)
-%         break;
-%     else
-%         pname = pname(possep+1:end);
-%     end
-% end
-%figName = strrep(pname,'_',' '); % Versioning with folder structure 2018-03-07
 
 % build MASH-FRET graphical interface
 h_fig = buildMASHfig;
