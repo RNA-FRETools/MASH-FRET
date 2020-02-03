7cc3793cd5ade4a0f6aa592f831d1f36f7241de1
mca-sh <melodie.hadzic@protonmail.com>
2020-02-03 18:55:09 +0100

    Review FRET and stoichiometry calculations
    - **review** FRET and stoichiometry calculations for mutiple FRET pairs by taking into account gamma factors of other FRET pairs and removing dependency on sym toolbox (increases calculation speed!)

--- MASH-FRET/source/mod-trace-processing/FRET/calcS.m
+++ MASH-FRET/source/mod-trace-processing/FRET/calcS.m
@@ -1,22 +1,64 @@
-function S_tr = calcS(exc, chanExc, S, FRET, I_den, gamma, beta)
+function S_tr = calcS(exc, chanExc, S, p, I, g, b)
 
 nS = size(S,1);
-L = size(I_den,1);
-S_tr = nan(L,nS);
+L = size(I,1);
 
-for s = 1:nS
-    don = S(s,1);
-    acc = S(s,2);
-    ldon = find(exc==chanExc(don),1);
-    lacc = find(exc==chanExc(acc),1);
-    
-    fret = find(FRET(:,1)==don & FRET(:,2)==acc);
+% initializes output
+S_tr = NaN(L,nS);
 
-    I_DD = I_den(:,don,ldon);
-    I_othersD = sum(I_den(:,[1:(don-1),(don+1):end],ldon),2);
-    I_AA = I_den(:,acc,lacc);
-    I_othersA = sum(I_den(:,[1:(acc-1),(acc+1):end],lacc),2);
-    
-    S_tr(:,s) = (gamma(:,fret).*I_DD+I_othersD)./(gamma(:,fret).*I_DD+...
-        I_othersD+I_AA./beta(:,fret)+I_othersA);
+% get emitters
+donors = unique(p(:,1),'stable')';
+[o,redOrder] = sort(chanExc(donors),'descend');
+donors = donors(redOrder);
+[emitters,id,o] = unique(p,'stable');
+K = numel(emitters);
+p_0 = p;
+for k = 1:K
+    p(p_0==p_0(id(k))) = k;
 end
+
+% reshape correction factors
+beta = ones(L,K,K);
+gamma = ones(L,K,K);
+for pair = 1:nS
+    d = p(pair,1);
+    a = p(pair,2);
+    beta(:,emitters==d,emitters==a) = b(:,pair);
+    gamma(:,emitters==d,emitters==a) = g(:,pair);
+end
+
+% get gamma-corrected FRET expression
+S_expr = cell(K,K);
+for idd = 1:(K-1)
+    for ida = (idd+1:K)
+        S_expr{idd,ida} = buildSExpr(idd,ida,K);
+        S_expr{idd,ida} = replaceByArrays(S_expr{idd,ida},K,chanExc,exc);
+    end
+end
+
+S_mat = NaN(L,K,K);
+
+% solve Stoichiometry expression from reddest to greenest donor
+for d = donors
+    acceptors = p(p(:,1)==d,2)';
+    for a = acceptors
+        if isempty(S_expr{emitters==d,emitters==a}) || chanExc(d)==0  || ...
+                chanExc(a)==0
+            continue
+        end
+        S_mat(:,emitters==d,emitters==a) = ...
+            eval(S_expr{emitters==d,emitters==a});
+    end
+end
+
+% reorder FRET data
+for pair = 1:nS
+    d = p(pair,1);
+    a = p(pair,2);
+    S_tr(:,pair) = S_mat(:,emitters==d,emitters==a);
+end
+
+S_tr(isnan(S_tr))=0;
+
+
+
