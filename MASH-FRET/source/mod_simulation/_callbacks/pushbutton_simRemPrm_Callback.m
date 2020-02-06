72696eb6c70cb6e61185c3dd03fc99a98d3ffb4e
mca-sh <melodie.hadzic@protonmail.com>
2020-02-06 23:05:15 +0100

    correct preset import
    - **fix** error occurring when importing FRET config withtout corresponding transition rates with a number of states larger than 5
    - **fix** error occurring when importing successively presets with increasing number of molecules

--- MASH-FRET/source/mod_simulation/_callbacks/pushbutton_simRemPrm_Callback.m
+++ MASH-FRET/source/mod_simulation/_callbacks/pushbutton_simRemPrm_Callback.m
@@ -9,54 +9,10 @@ function pushbutton_simRemPrm_Callback(obj, evd, h_fig)
 % update by MH, 12.12.2019:
 % >> update coordinates after removing pre-sets
 
-% defaults
-Jmax = 5; % maximum number of states allowed in GUI
-
-% collect parameters
-h = guidata(h_fig);
-p = h.param.sim;
-
-if isfield(p.molPrm, 'nbStates')
-    % adjust number of states to maximum allowed
-    if h.param.sim.nbStates>Jmax
-        set(h.edit_nbStates,'string',num2str(Jmax));
-        edit_nbStates_Callback(h.edit_nbStates,[],h_fig);
-        h = guidata(h_fig);
-        p = h.param.sim;
-    end
-end
-
-if isfield(p.molPrm, 'coord')
-    if ~(isempty(p.molPrm.coord) && ~isempty(p.coordFile))
-        % clear coordinates and PSF factorization matrix if imported from preset file
-        p.coord = [];
-        p.matGauss = cell(1,4);
-        
-        % set default to random coordinates
-        p.genCoord = 1;
-    else
-        % resort coordinates imported from ASCII file (sample size can increases)
-        p = resetSimCoord(p,h_fig);
-    end
-end
-
-if isfield(p.molPrm, 'psf_width')
-    % keep PSF widths of the first molecule as simulation parameters
-    p.PSFw = p.PSFw(1,:);
-    % clear PSF factorization matrix
-    p.matGauss = cell(1,4);
-end
-
-% reset parameters of presets file
-p.molPrm = [];
-p.impPrm = 0;
-p.prmFile = [];
-
-% save changes
-h.param.sim = p;
-guidata(h_fig, h);
+resetSimPrm(h_fig)
 
 % display potentially new coordinates
+h = guidata(h_fig);
 setSimCoordTable(h.param.sim,h.uitable_simCoord);
 
 % set GUI to proper values
