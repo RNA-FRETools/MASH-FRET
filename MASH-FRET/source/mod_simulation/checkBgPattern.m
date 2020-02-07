3cd7a8b6ab3f1fb0f35200939e0a6218e0d4d32c
mca-sh <melodie.hadzic@protonmail.com>
2020-02-07 10:37:52 +0100

    improve Simulation's "pattern" background
    - **fix error** occuring when improting background image for simulation
    - **modify GUI** by adding an import interface for background image

--- MASH-FRET/source/mod_simulation/checkBgPattern.m
+++ MASH-FRET/source/mod_simulation/checkBgPattern.m
@@ -7,57 +7,30 @@ function [ok, p] = checkBgPattern(p, h_fig)
 
 ok = 1;
 
-if ~isfield(p, 'bgImg') || isempty(p.bgImg)
-    data = getFile2sub('Pick a BG pattern image', h_fig);
-    if isempty(data)
-        ok = 0;
-        return;
+if isfield(p, 'bgImg') && ~isempty(p.bgImg)
+    return
+end
+
+data = getFile2sub('Pick a BG pattern image', h_fig);
+if isempty(data)
+    ok = 0;
+    return
+end
+if strcmp(p.intUnits, 'electron')
+    [o,K,eta] = getCamParam(p.noiseType,p.camNoise);
+    data.frameCur = ele2phtn(data.frameCur,K,eta);
+    if size(data.frameCur,3)>1
+        data.frameCur = sum(data.frameCur,3);
     end
-    if strcmp(p.intUnits, 'electron')
-        [mu_y_dark,K,eta] = getCamParam(p.noiseType,p.camNoise);
-        data.frameCur = ele2phtn(data.frameCur,K,eta);
-        if size(data.frameCur,3)>1
-            data.frameCur = sum(data.frameCur,3);
-        end
-        if size(data.frameCur,1)~=p.movDim(1) || ...
-                size(data.frameCur,2)~=p.movDim(2)
-            setContPan(cat(2,'Dimensions of the background image are not ',...
-                'consistent with video dimensions: please adjust video ',...
-                'dimensions or modify the dimensions of the background ',...
-                'image.'),'error',h_fig);
-            ok = 0;
-            return;
-        end
-    end
-    p.bgImg = data;
-else
-    loadBG = questdlg('Load another BG pattern?', ...
-        'Background image', 'Yes', 'No', 'No');
-    if isempty(loadBG)
+    if size(data.frameCur,1)~=p.movDim(1) || size(data.frameCur,2)~=p.movDim(2)
+        setContPan(cat(2,'Dimensions of the background image are not ',...
+            'consistent with video dimensions: please adjust video ',...
+            'dimensions or modify the dimensions of the background ',...
+            'image.'),'error',h_fig);
         ok = 0;
-        return;
-    elseif strcmp(loadBG, 'Yes')
-        data = getFile2sub('Pick a BG pattern image', h_fig);
-        if isempty(data)
-            ok = 0;
-            return;
-        end
-        if strcmp(p.intUnits, 'electron')
-            [mu_y_dark,K,eta] = getCamParam(p.noiseType,p.camNoise);
-            data.frameCur = ele2phtn(data.frameCur,K,eta);
-            if size(data.frameCur,3)>1
-                data.frameCur = sum(data.frameCur,3);
-            end
-            if size(data.frameCur,1)~=p.movDim(1) || ...
-                    size(data.frameCur,2)~=p.movDim(2)
-                setContPan(cat(2,'Dimensions of the background image are ',...
-                    'not consistent with video dimensions: please adjust ',...
-                    'video dimensions or modify the dimensions of the ',...
-                    'background image.'),'error',h_fig);
-                ok = 0;
-                return;
-            end
-        end
-        p.bgImg = data;
+        return
     end
 end
+
+p.bgImg = data;
+
--- MASH-FRET/source/mod_simulation/ud_S_expSetupPan.m
+++ MASH-FRET/source/mod_simulation/ud_S_expSetupPan.m
@@ -14,6 +14,7 @@ p = h.param.sim;
 
 % set all controls on-enabled
 setProp(h.uipanel_S_experimentalSetup,'enable','on');
+setProp(h.uipanel_S_experimentalSetup,'visible','on');
 
 % reset background color of edit fields
 set([h.edit_psfW1 h.edit_psfW2 h.edit_simzdec h.edit_simz0_A ...
@@ -36,32 +37,7 @@ else
     end
 end
 
-% set background spatial distribution
-set(h.popupmenu_simBg_type, 'Value', p.bgType);
-if strcmp(p.intUnits, 'electron')
-    [offset,K,eta] = getCamParam(p.noiseType,p.camNoise);
-    p.bgInt_don = phtn2ele(p.bgInt_don,K,eta);
-    p.bgInt_acc = phtn2ele(p.bgInt_acc,K,eta);
-end
-set(h.edit_bgInt_don, 'String', num2str(p.bgInt_don));
-set(h.edit_bgInt_acc, 'String', num2str(p.bgInt_acc));
-if p.bgType == 2
-    set(h.edit_TIRFx, 'String', num2str(p.TIRFdim(1)));
-    set(h.edit_TIRFy, 'String', num2str(p.TIRFdim(2)));
-else
-    set([h.edit_TIRFx h.edit_TIRFy h.text_simWTIRF], 'Enable', 'off');
-    set([h.edit_TIRFx h.edit_TIRFy], 'String', '');
-end
-
-% set dynamic background
-set(h.checkbox_bgExp, 'Value', p.bgDec);
-if p.bgDec
-    set(h.edit_bgExp_cst, 'String', num2str(p.cstDec));
-    set(h.edit_simAmpBG, 'String', num2str(p.ampDec));
-else
-    set([h.edit_bgExp_cst h.edit_simAmpBG], 'Enable', 'off', 'String', '');
-end
-
+% set defocusing
 set(h.checkbox_defocus,'value',defocus);
 if defocus
     set(h.edit_simzdec, 'String', num2str(p.zDec));
@@ -70,5 +46,50 @@ else
     set([h.text_simz0 h.text_simzdec h.edit_simzdec h.edit_simz0_A], ...
         'Enable', 'off');
     set([h.edit_simzdec h.edit_simz0_A], 'String', '');
+end
+
+% set background spatial distribution
+set(h.popupmenu_simBg_type, 'Value', p.bgType);
+
+if p.bgType~=3 % constant or TIRF
+    set ([h.pushbutton_S_impBgImg,h.text_S_bgImgFile,h.edit_S_bgImgFile], ...
+        'Enable', 'off', 'Visible', 'off');
+    
+    if strcmp(p.intUnits, 'electron')
+        [offset,K,eta] = getCamParam(p.noiseType,p.camNoise);
+        p.bgInt_don = phtn2ele(p.bgInt_don,K,eta);
+        p.bgInt_acc = phtn2ele(p.bgInt_acc,K,eta);
+    end
+    set(h.edit_bgInt_don, 'String', num2str(p.bgInt_don));
+    set(h.edit_bgInt_acc, 'String', num2str(p.bgInt_acc));
+    
+    if p.bgType==2 % TIRF
+        set(h.edit_TIRFx, 'String', num2str(p.TIRFdim(1)));
+        set(h.edit_TIRFy, 'String', num2str(p.TIRFdim(2)));
+    else % constant
+        set([h.edit_TIRFx,h.edit_TIRFy,h.text_simWTIRF,h.text_simWTIRF_x, ...
+            h.text_simWTIRF_y], 'Enable', 'off', 'Visible', 'off');
+    end
+    
+else % pattern
+    set([h.edit_bgInt_don,h.edit_bgInt_acc,h.text_simBgD,h.text_simBgA,...
+        h.edit_TIRFx,h.edit_TIRFy,h.text_simWTIRF,h.text_simWTIRF_x,...
+        h.text_simWTIRF_y], 'Enable', 'off', 'Visible', 'off');
+    
+    if isfield(p,'bgImg') && isfield(p.bgImg,'file') && ...
+            ~isempty(p.bgImg.file)
+        [o,fname,fext] = fileparts(p.bgImg.file);
+        set(h.edit_S_bgImgFile,'string',[fname,fext]);
+    end
+end
+
+% set dynamic background
+set(h.checkbox_bgExp, 'Value', p.bgDec);
+if p.bgDec
+    set(h.edit_bgExp_cst, 'String', num2str(p.cstDec));
+    set(h.edit_simAmpBG, 'String', num2str(p.ampDec));
+else
+    set([h.edit_bgExp_cst h.edit_simAmpBG h.text_dec h.text_amp], 'Enable', ...
+        'off', 'String', '');
 end
 
