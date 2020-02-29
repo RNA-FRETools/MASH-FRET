f6d8bc08f980458cb77be83bce27853e73e9dcb7
mca-sh <melodie.hadzic@protonmail.com>
2020-02-29 05:04:04 +0100

    Update updateTAplots.m

--- MASH-FRET/source/mod_transition_analysis/management/updateTAplots.m
+++ MASH-FRET/source/mod_transition_analysis/management/updateTAplots.m
@@ -1,4 +1,20 @@
-function updateTAplots(h_fig)
+function updateTAplots(h_fig,varargin)
+% updateTAplots(h_fig)
+% updateTAplots(h_fig,'all')
+% updateTAplots(h_fig,'tdp')
+% updateTAplots(h_fig,'kin')
+%
+% Refresh plots in Transition analysis
+%
+% h_fig: handle to main figure
+% 'all': refresh all plots
+% 'tdp': refresh TDP plot only
+% 'kin': refresh dwell time histogram only
+
+opt = 'all';
+if ~isempty(varargin)
+    opt = varargin{1};
+end
 
 h = guidata(h_fig);
 p = h.param.TDP;
@@ -17,6 +33,10 @@ curr = p.proj{proj}.curr{tag,tpe};
 prm = p.proj{proj}.prm{tag,tpe};
 curr_k = curr.kin_start{2}(2);
 
-plotTDP([h.axes_TDPplot1,h.colorbar_TA,h.axes_tdp_BIC], curr, prm);
-plotKinFit(h.axes_TDPplot2, p, prm, tag, tpe, curr_k,...
-    get(h.pushbutton_TDPfit_log, 'String'))
+if strcmp(opt,'all') || strcmp(opt,'tdp')
+    plotTDP([h.axes_TDPplot1,h.colorbar_TA,h.axes_tdp_BIC], curr, prm);
+end
+if strcmp(opt,'all') || strcmp(opt,'kin')
+    plotKinFit(h.axes_TDPplot2, p, prm, tag, tpe, curr_k,...
+        get(h.pushbutton_TDPfit_log, 'String'))
+end
