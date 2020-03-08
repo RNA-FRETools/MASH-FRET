e8c0b1db8692fe514ea1efa7a8a678981a41ec65
mca-sh <melodie.hadzic@protonmail.com>
2020-03-08 17:49:30 +0100

    Update pushbutton_exportSim_Callback.m
    - **review** simulation export by not updating data prior exporting (not necessary, time consumming and incompatible with kinsoft routine when removin f externally blurr states from FRET sequences

--- MASH-FRET/source/mod_simulation/_callbacks/pushbutton_exportSim_Callback.m
+++ MASH-FRET/source/mod_simulation/_callbacks/pushbutton_exportSim_Callback.m
@@ -3,23 +3,23 @@ function ok = pushbutton_exportSim_Callback(obj, evd, h_fig)
 % set fields to proper values
 updateFields(h_fig, 'sim');
 
-% Check for correct patterned background image
-h = guidata(h_fig);
-if h.param.sim.bgType == 3 % pattern
-    p = h.param.sim;
-    [ok,p] = checkBgPattern(p, h_fig);
-    if ~ok
-        return
-    end
-    h.param.sim = p;
-    guidata(h_fig, h);
-end
+% % Check for correct patterned background image
+% h = guidata(h_fig);
+% if h.param.sim.bgType == 3 % pattern
+%     p = h.param.sim;
+%     [ok,p] = checkBgPattern(p, h_fig);
+%     if ~ok
+%         return
+%     end
+%     h.param.sim = p;
+%     guidata(h_fig, h);
+% end
 
-% Calculate intensity state sequences and generate coordinates
-[ok,actstr1] = updateMov(h_fig);
-if ~ok
-    return
-end
+% % Calculate intensity state sequences and generate coordinates
+% [ok,actstr1] = updateMov(h_fig);
+% if ~ok
+%     return
+% end
 
 if iscell(obj)
     pname = obj{1};
@@ -36,8 +36,10 @@ if ~ok
 end
 
 % update actions
+% setContPan(cat(2,'Success! Simulated data have been exported to files:',...
+%     actstr1,actstr2),'success',h_fig);
 setContPan(cat(2,'Success! Simulated data have been exported to files:',...
-    actstr1,actstr2),'success',h_fig);
+    actstr2),'success',h_fig);
 
 % set fields to proper values
 updateFields(h_fig, 'sim');
