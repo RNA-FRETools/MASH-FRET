0b7932618df7aa29e3876d19487d90af60230d8b
mca-sh <melodie.hadzic@protonmail.com>
2020-03-21 11:41:31 +0100

    Update buildModel.m
    - **add** user-defined initial state probabilities
    - restore old comments

--- MASH-FRET/source/mod_simulation/management/buildModel.m
+++ MASH-FRET/source/mod_simulation/management/buildModel.m
@@ -4,17 +4,10 @@ function ok = buildModel(h_fig)
 %
 % Requires external files: setContPan.m
 
-% Last update by MH, 19.12.2019
-% >> fix error occuring when the sum of one of the rows/columns in the rate
-%  matrix is null
-% >> return execution success/failure in "ok"
-%
-% update by MH, 17.12.2019
-% >> remove dependency on updateMov.m (called from the pushbutton callback 
-% function)
-%
-% update by RB, 6.3.2018
-% >> comment: To be checked 2018-03-06 
+% Last update, 17.3.2020: (1) use pre-defined transition probabilties (wx) (2) use HMM transition probabiltiies to calculate initial state probabilities ((kx*expT).*wx) (3) add possibility to use pre-defined initial state probabilties (p0)
+% update by MH, 19.12.2019: (1) fix error occuring when the sum of one of the rows/columns in the rate matrix is null (2) return execution success/failure in "ok"
+% update by MH, 17.12.2019: remove dependency on updateMov.m (called from the pushbutton callback function)
+% update by RB, 6.3.2018: review initial state probabilities
 
 % initialize execution failure/success
 ok = 0;
@@ -32,14 +25,22 @@ K = h.param.sim.nbStates;
 impPrm = h.param.sim.impPrm;
 molPrm = h.param.sim.molPrm;
 imp_kx = impPrm & isfield(molPrm, 'kx');
+imp_wx = impPrm & isfield(molPrm, 'wx');
+imp_p0 = impPrm & isfield(molPrm, 'p0');
 
 if imp_kx
     kx_all = molPrm.kx;
-    wx_all = molPrm.wx;
 else
     kx = h.param.sim.kx;
-    wx = h.param.sim.wx;
     kx_all  = repmat(kx,[1,1,n_max]);
+end
+if imp_wx
+    wx_all = molPrm.wx;
+else
+    wx = h.param.sim.wx;
+    if size(wx,2)<K
+        wx = ones(K);
+    end
     wx_all = repmat(wx,[1,1,n_max]);
 end
 ref = ~~(kx_all);
@@ -53,12 +54,17 @@ dt_final = cell(1,n_max);
 if K>1 && isTrans
     n = 1;
     while n <= n_max
-        if n==1
-            kx = kx_all(1:K,1:K,n);
-            wx = wx_all(1:K,1:K,n);
-            wx = wx./repmat(sum(wx,2),[1,K]);
-
-            % identify zero sums in rate matrix
+        kx = kx_all(1:K,1:K,n);
+        kx(~~eye(size(kx))) = 0;
+        wx = wx_all(1:K,1:K,n);
+        wx(~~eye(size(wx))) = 0;
+        wx = wx./repmat(sum(wx,2),[1,K]);
+        if imp_p0
+            % MH 17.3.2020: pre-defined initial state probabilities
+            Prob = molPrm.p0(n,:);
+            
+        else
+            % MH 19.12.2019: identify zero sums in rate matrix
             if sum(sum(kx,1)>0)~=K || sum(sum(kx,2)>0)~=K
                 setContPan(cat(2,'Simulation aborted: at least one ',...
                     'transition from and to each state must be defined ',...
@@ -72,21 +78,20 @@ if K>1 && isTrans
                 return
             end
 
-            %             P = kx*N; // M.CAS Hadzic 2012
-            %             P = double(~~(kx)); %  M.CAS Hadzic 2014, to be updated
+            % initial state probabilities from rates
+%             P = kx*N; // M.CAS Hadzic 2015
+%             P = double(~~(kx)); %  M.CAS Hadzic 2014, to be updated
 %             P = kx.*expT; % simple rate-to-prob-model for Markov chains, 2018-03-12 RB
-            P = (kx*expT).*wx; % use probability-weighted rates
+            P = (kx*expT).*wx; % MH 17.3.2020: use HMM transition probabilities
             for i=1:K 
-                 %P(:,i) = (kx(:,i)./sum(kx,2)(i))*(1-exp(-sum(kx,2)(i)*expT));
-%                  P(i,i) = 1 - sum(kx(i,:))*expT; % simple rate-to-prob-model for Markov chains, 2018-03-12 RB
-                P(i,i) = 1 - sum(P(i,:));
+%                 P(:,i) = (kx(:,i)./sum(kx,2)(i))*(1-exp(-sum(kx,2)(i)*expT)); % 2018-03-12 RB PlosOne
+%                 P(i,i) = 1 - sum(kx(i,:))*expT; % simple rate-to-prob-model for Markov chains, 2018-03-12 RB
+                P(i,i) = 1 - sum(P(i,:)); % use HMM transition probabilities, MH 17.3.2020
             end
-            % State probabilities from rates, 2018-03-12 RB PlosOne
             [V,D,W]=eig(P);
             D = diag(D);
             for i=1:K
                 if round(1e5*real(D(i)))/1e5==1 && round(imag(D(i)))==0 
-                    %disp(i) % control, 2018-03-12 RB
                     break   
                 end
             end
@@ -112,8 +117,7 @@ if K>1 && isTrans
             % pick a "first state" randomly with uniform probabilities
             %s_curr = randsample(1:K, 1, true, double(~~(kx))); % M.CAS Hadzic 2014
             
-            % pick a "first state" randomly weighted with state
-            % propabilities 
+            % pick a "first state" randomly weighted with state propabilities 
             s_curr = randsample(1:K, 1, true, Prob); % 2018-03-12, RB PlosOne
           
             while t<end_t
@@ -126,11 +130,17 @@ if K>1 && isTrans
                 end
                 
                 s_prev = s_curr; % previous state
-
+                
+                 % pick a "next state" randomly with uniform probabilities
+                %s_curr = randsample(1:K, 1, true, double(~~P(s_prev,:)));
+                
+                % pick a "next state" randomly with P-based probabilities
+                %s_curr = randsample(1:K, 1, true, P(s_prev,:));
+                
                 % pick a "next state" randomly with k-based probabilities
 %                 s_curr = randsample(1:K, 1, true, kx(s_prev,:)/sum(kx(s_prev,:)));
                 
-                % pick a "next state" randomly with defined probabilities
+                % pick a "next state" randomly with defined probabilities, MH 17.3.2020
                 s_curr = randsample(1:K, 1, true, wx(s_prev,:));
                 
                 % dwell in s
@@ -197,19 +207,25 @@ else
     for n = 1:n_max
         mix{n} = zeros(K,N);
         if K > 1
-            % pick a "first state" randomly
-            if n==1 && ~imp_kx
-                kx = kx(1:K,1:K);
-                P = kx*N;
-                % pick a "first state" randomly with uniform probabilities
-                s_curr = randsample(1:K, 1, true, sum(P,1));
+            if imp_p0
+                % MH 17.3.2020: pre-defined initial state probabilities
+                Prob = molPrm.p0(n,:);
 
-            elseif imp_kx
-                kx = kx_all(1:K,1:K,n);
-                P = kx*N;
-                % pick a "first state" randomly with uniform probabilities
-                s_curr = randsample(1:K, 1, true, sum(P,1));
+            else
+                % MH 19.12.2019: identify zero sums in rate matrix
+                setContPan(cat(2,'Simulation aborted: when no transition ',...
+                    'is defined (null rates), initial state probabilities',...
+                    ' must be pre-defined.'),'error',h_fig);
+
+                % clear any results to avoid conflict
+                if isfield(h,'results') && isfield(h.results,'sim')
+                    h.results = rmfield(h.results,'sim');
+                end
+                guidata(h_fig,h);
+                return
             end
+            % pick a "first state" randomly
+            s_curr = randsample(1:K, 1, true, sum(Prob,1));
         else
             s_curr = 1;
         end
