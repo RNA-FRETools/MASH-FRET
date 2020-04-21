29b2b92cf93c435bb13130b685b4de71468297f4
mca-sh <melodie.hadzic@protonmail.com>
2020-04-21 18:11:12 +0200

    improve cluster selection with mouse (2)

--- MASH-FRET/source/divers/positions/posAxesToFig.m
+++ MASH-FRET/source/divers/positions/posAxesToFig.m
@@ -7,20 +7,8 @@ lim_y = get(h_axes,'ylim');
 pos_un(1) = (pos(1)-lim_x(1))*posaxes(3)/(lim_x(2)-lim_x(1));
 pos_un(2) = (pos(2)-lim_y(1))*posaxes(4)/(lim_y(2)-lim_y(1));
 
-% convert to figure-referenced pixel position
-h_p = h_axes;
-while h_p~=h_fig && isprop(h_p,'parent')
-    posparent = getPixPos(h_p);
-    pos_un(1) = pos_un(1)+posparent(1);
-    pos_un(2) = pos_un(2)+posparent(2);
-    h_p = get(h_p,'parent');
-end
+% convert to figure-referenced pixel position and input units
+pos_un = posChildToFig(pos_un,h_fig,h_axes,units);
 
-% convert to input units
-if strcmp(units,'normalized')
-    posfig = getPixPos(h_fig);
-    pos_un(1) = pos_un(1)/posfig(3);
-    pos_un(2) = pos_un(2)/posfig(4);
-end
 
 
