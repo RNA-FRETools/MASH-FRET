0809b6005792bba6396f2f2dba914f9c54947eaf
Mélodie Hadzic <melodiehadzic@protonmail.com>
2020-11-16 07:48:41 +0100

    Update MASH.m
    display logs when starting MASH

--- MASH-FRET/MASH.m
+++ MASH-FRET/MASH.m
@@ -1,6 +1,7 @@
 function h_fig = MASH(varargin)
 
 % add source folders to MATLAB search path
+disp('set MATLAB search path...')
 codePath = fileparts(mfilename('fullpath'));
 addpath(genpath(codePath));
 
@@ -8,15 +9,18 @@ addpath(genpath(codePath));
 feature('DefaultCharacterSet','windows-1252');
 
 % get MATLAB version
+disp('get MASH-FRET version...')
 version_str = getMASHversion(codePath);
 
 % define figure name from MASH-FRET version
 figName = sprintf('%s %s','MASH-FRET', version_str);
 
 % build MASH-FRET graphical interface
+disp('build GUI...')
 h_fig = buildMASHfig(figName);
 
 % initialize main figure
+disp('initialize GUI..')
 initMASH(h_fig);
 
 % make main figure visible
