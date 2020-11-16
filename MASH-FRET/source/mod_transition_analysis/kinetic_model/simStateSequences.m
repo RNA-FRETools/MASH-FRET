3657f245c87c28bd473e03b5e874a825cd88ef91
Mélodie Hadzic <melodiehadzic@protonmail.com>
2020-11-16 07:43:12 +0100

    Update simStateSequences.m
    use frame-wise simulation instead of dwell time wise and return initial state probabibilties (used for forward ptobabilities calculatons)

--- MASH-FRET/source/mod_transition_analysis/kinetic_model/simStateSequences.m
+++ MASH-FRET/source/mod_transition_analysis/kinetic_model/simStateSequences.m
@@ -31,7 +31,8 @@ end
 
 % calculate initial state probabilities
 if isempty(r)
-    r = -log(tp(~~eye(size(tp))));
+%     r = -log(diag(tp));
+    r = sum(tp,2)-diag(tp);
 end
 r(r==0) = 1E-6;
 if isempty(ip)
@@ -52,8 +53,8 @@ if isempty(ip)
 end
 
 Js = 1:J;
-w = zeros(size(tp));
-w(~eye(size(tp))) = tp(~eye(size(tp)));
+w = tp;
+w(~~eye(size(tp))) = 0;
 w = w./repmat(sum(w,2),[1,J]);
 w(isnan(w)) = 0;
 w(:,ip==0) = 0;
@@ -63,57 +64,57 @@ L = sum(Ls);
 state1 = randsample(1:J,1,true,ip);
 
 % generate state sequence in a frame-wise fashion
-% for j = 1:J
-%     j2s = Js(Js~=j);
-%     tp(j,j) = 1-exp(-sum(tp(j,j2s),2));
-% end
-% seq = NaN(L,1);
-% seq(1,1) = state1;
-% for l = 2:L
-%     draw1 = rand(1);
-%     istrans = draw1<=tp(state1,state1);
-%     if istrans
-%         draw2 = rand(1);
-%         states2 = Js(Js~=state1);
-%         id = find(cumsum(w(state1,states2))>draw2);
-%         state2 = states2(id(1));
-% %         state2 = randsample(1:J,1,true,tp(state1,:));
-%     else
-%         state2 = state1;
-%     end
-%     seq(l,1) = state2;
-%     state1 = state2;
-% end
-    
-% generate state sequence in a dwell time-wise fashion
-dt = [];
-l = 0;
-while l<L
-    if ~isinf(r(state1))
-        % draw a dwell time (in number of data points)
-        dl = random('exp',1/(r(state1)));
-        if (l+dl)>L
-            dl = L-l;
-        end
-        l = l+dl;
-    end
-    if sum(w(state1,:))==0
-        state2 = state1;
-    else
-        draw = rand(1);
+for j = 1:J
+    j2s = Js(Js~=j);
+    tp(j,j) = 1-exp(-sum(tp(j,j2s),2));
+end
+seq = NaN(L,1);
+seq(1,1) = state1;
+for l = 2:L
+    draw1 = rand(1);
+    istrans = draw1<=tp(state1,state1);
+    if istrans
+        draw2 = rand(1);
         states2 = Js(Js~=state1);
-        id = find(cumsum(w(state1,states2))>draw);
-        if isempty(id)
-            state2 = states2(end);
-        else
-            state2 = states2(id(1));
-        end
-%         state2 = randsample(1:J,1,true,w(state1,:));
+        id = find(cumsum(w(state1,states2))>draw2);
+        state2 = states2(id(1));
+%         state2 = randsample(1:J,1,true,tp(state1,:));
+    else
+        state2 = state1;
     end
-    dt = cat(1,dt,[dl,state1,state2]);
+    seq(l,1) = state2;
     state1 = state2;
 end
-seq = getDiscrFromDt(dt,1);
+    
+% generate state sequence in a dwell time-wise fashion
+% dt = [];
+% l = 0;
+% while l<L
+%     if ~isinf(r(state1))
+%         % draw a dwell time (in number of data points)
+%         dl = random('exp',1/(r(state1)));
+%         if (l+dl)>L
+%             dl = L-l;
+%         end
+%         l = l+dl;
+%     end
+%     if sum(w(state1,:))==0
+%         state2 = state1;
+%     else
+%         draw = rand(1);
+%         states2 = Js(Js~=state1);
+%         id = find(cumsum(w(state1,states2))>draw);
+%         if isempty(id)
+%             state2 = states2(end);
+%         else
+%             state2 = states2(id(1));
+%         end
+% %         state2 = randsample(1:J,1,true,w(state1,:));
+%     end
+%     dt = cat(1,dt,[dl,state1,state2]);
+%     state1 = state2;
+% end
+% seq = getDiscrFromDt(dt,1);
 
 % get dwell times from state sequences
 dt = [];
@@ -127,7 +128,7 @@ for n = 1:N
 end
 
 % recover experimental probabilities and transition numbers
-tp_exp = zeros(J);
+k_exp = zeros(J);
 n_exp = zeros(J);
 for j1 = 1:J
     dt_j1 = dt(dt(:,3)==j1,:);
@@ -137,13 +138,19 @@ for j1 = 1:J
         end
         dt_j1j2 = dt_j1(dt_j1(:,4)==j2,1);
         n_exp(j1,j2) = numel(dt_j1j2);
-        tp_exp(j1,j2) = n_exp(j1,j2)/sum(dt_j1(:,1));
+        k_exp(j1,j2) = n_exp(j1,j2)/sum(dt_j1(:,1));
     end
 end
-res.k_exp = tp_exp;
-tp_exp(~~eye(size(tp_exp))) = 1-sum(tp_exp,2);
+if size(ip,1)>1
+    ip = ip';
+end
+res.ip = ip;
 res.dt = dt;
-res.tp_exp = tp_exp;
-res.w_exp = n_exp./repmat(sum(n_exp,2),[1,J]);
+res.k_exp = k_exp;
 res.n_exp = n_exp;
+res.w_exp = n_exp./repmat(sum(n_exp,2),[1,J]);
+res.tp_exp = k_exp;
+res.tp_exp(~~eye(size(k_exp))) = 1-sum(k_exp,2);
+
+
 
