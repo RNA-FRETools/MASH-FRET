ad20b057d75fbbb5f9f83aa763eaeb80d7c0e986
mca-sh <melodiehadzic@protonmail.com>
2021-09-20 22:43:04 +0200

    accelerate the reading of tif files

--- MASH-FRET/source/mod_video_processing/graphic_files/readTif_OME.m
+++ MASH-FRET/source/mod_video_processing/graphic_files/readTif_OME.m
@@ -22,33 +22,33 @@ if ~useMov
     isMov = 0;
 end
 
-% get video infos
-info = imfinfo(fullFname); % information array of .tif file
-frameLen = numel(info); % number total of frames    
-txt = ['arbitrary ' num2str(cycleTime)];
-txt_exc = '';
-if isfield(info(1,1),'ImageDescription')
-    descr = info(1,1).ImageDescription;
-    [cycleTime,lord,exc] = getTIFOME_metadata(descr);
-    if ~isempty(cycleTime) && ~isnan(cycleTime)
-        txt = num2str(cycleTime);
-    end
-    if ~isempty(exc)
-        txt_exc = 'Lasers = ';
-        for l = 1:numel(exc)
-            if l==1
-                txt_exc = cat(2,txt_exc,num2str(exc(l)),'nm');
-            else
-                txt_exc = cat(2,txt_exc,', ',num2str(exc(l)),'nm');
-            end
-        end
-        txt_exc = cat(2,txt_exc,'\n');
-    end
-end
-pixelY = info(1,1).Width; % height of the movie after 90° rotation
-pixelX = info(1,1).Height; % width of the movie after 90° rotation
-
 if isempty(fDat)
+    % get video infos
+    fCurs = imfinfo(fullFname); % information array of .tif file
+    frameLen = numel(fCurs); % number total of frames    
+    txt = ['arbitrary ' num2str(cycleTime)];
+    txt_exc = '';
+    if isfield(fCurs(1,1),'ImageDescription')
+        descr = fCurs(1,1).ImageDescription;
+        [cycleTime,lord,exc] = getTIFOME_metadata(descr);
+        if ~isempty(cycleTime) && ~isnan(cycleTime)
+            txt = num2str(cycleTime);
+        end
+        if ~isempty(exc)
+            txt_exc = 'Lasers = ';
+            for l = 1:numel(exc)
+                if l==1
+                    txt_exc = cat(2,txt_exc,num2str(exc(l)),'nm');
+                else
+                    txt_exc = cat(2,txt_exc,', ',num2str(exc(l)),'nm');
+                end
+            end
+            txt_exc = cat(2,txt_exc,'\n');
+        end
+    end
+    pixelY = fCurs(1,1).Width; % height of the movie after 90° rotation
+    pixelX = fCurs(1,1).Height; % width of the movie after 90° rotation
+    
     if ~isempty(h_fig)
         updateActPan(['Tagged Image File Format(*.tiff)\n' ...
             'Cycle time = ' txt 's-1\n' ...
@@ -64,6 +64,12 @@ if isempty(fDat)
             'Movie length = ' num2str(frameLen) ' frames\n' ...
             txt_exc]);
     end
+else
+    fCurs = fDat{1}{1};
+    lord = fDat{1}{2};
+    frameLen = fDat{3};
+    pixelX = fDat{2}(1);
+    pixelY = fDat{2}(2);
 end
 
 if strcmp(n,'all')
@@ -96,7 +102,7 @@ if strcmp(n,'all')
             movie = zeros(pixelY,pixelX,frameLen);
             for i = 1:frameLen
                 movie(:,:,i) = double(imread(fullFname,'Index',i,'Info',...
-                    info))';   
+                    fCurs))';   
                 
                 if loading_bar('update', h_fig)
                     ok = 0;
@@ -111,7 +117,7 @@ if strcmp(n,'all')
             h.movie.movie = zeros(pixelY,pixelX,frameLen);
 
             for i = 1:frameLen
-                img = double(imread(fullFname,'Index',i,'Info',info));
+                img = double(imread(fullFname,'Index',i,'Info',fCurs));
                 if size(img,3)>1
                     img = sum(img,3);
                 end
@@ -133,7 +139,7 @@ else
     if isMov==2
         frameCur = h.movie.movie(:,:,n);
     else
-        frameCur = double(imread(fullFname,'Index',lord(n),'Info',info))';
+        frameCur = double(imread(fullFname,'Index',lord(n),'Info',fCurs))';
         if size(frameCur,3)>1
             frameCur = sum(frameCur,3);
         end
@@ -145,9 +151,9 @@ data = struct('cycleTime', cycleTime, ...
               'pixelY', pixelY, ...
               'pixelX', pixelX, ...
               'frameLen', frameLen, ...
-              'fCurs', [], ...
               'frameCur', frameCur, ...
               'movie', movie);
+data.fCurs = {fCurs,lord};
 if ~isempty(exc)
     data.lasers = exc;
 end
