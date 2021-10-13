1314bf12af98a6e6f50947f648d62d8a83827462
mca-sh <melodiehadzic@protonmail.com>
2021-10-13 10:39:07 +0200

    manage colormaps when switching project/modules

--- MASH-FRET/source/bar-tool/_callbacks/switchPan.m
+++ MASH-FRET/source/bar-tool/_callbacks/switchPan.m
@@ -3,19 +3,26 @@ function switchPan(obj,evd,h_fig)
 h = guidata(h_fig);
 p = h.param;
 
+% defaults
 green = [0.76 0.87 0.78];
 grey = [240/255 240/255 240/255];
-
+minMap = 0;
+maxMap = 1;
+n_map = 50;
 mod = {'S','VP','TP','HA','TA'};
+
+% set button enability and color
 h_tb = [h.togglebutton_S,h.togglebutton_VP,h.togglebutton_TP,...
     h.togglebutton_HA,h.togglebutton_TA];
 h_pan = [h.uipanel_S,h.uipanel_VP,h.uipanel_TP,h.uipanel_HA,h.uipanel_TA];
-
 set(h_tb(h_tb==obj), 'BackgroundColor', green, 'Value', 1);
 set(h_tb(h_tb~=obj), 'BackgroundColor', grey, 'Value', 0);
+
+% set panel visibility
 set(h_pan(h_tb==obj), 'Visible', 'on');
 set(h_pan(h_tb~=obj), 'Visible', 'off');
 
+% display action
 if any(h_tb==obj) && ~strcmp(get(h_pan(h_tb==obj),'Visible'),'on')
     setContPan(['Module "',get(obj,'string'),'" selected.'],'none',h_fig);
 end
@@ -41,6 +48,20 @@ switch obj
             cmaps = get(h.popupmenu_colorMap, 'String');
             cmap = cmaps{p.proj{p.curr_proj}.VP.cmap};
             colormap(cmap);
+        end
+        
+    case h.togglebutton_TP
+        if isModuleOn(p,'TP')
+            proj = p.curr_proj;
+            brght = p.proj{proj}.TP.fix{1}(3); % [-1:1]
+            ctrst = p.proj{proj}.TP.fix{1}(4); % [-1:1]
+            x_map = linspace(minMap, maxMap, n_map);
+            a = 1+(ctrst)^3;
+            b = brght + (1-a)*0.5;
+            r = (a*x_map + b)';
+            r(r < 0) = 0;
+            r(r > 1) = 1;
+            colormap([r r r]);
         end
 
     case h.togglebutton_TA
