a21723fc18e776c394074621e4eaba3b17151a45
mca-sh <melodiehadzic@protonmail.com>
2021-11-06 10:07:58 +0100

    delete old project options

--- MASH-FRET/source/project/experiment-settings/project-options/_callbacks/figure_itgExpOpt_CloseRequestFcn.m
+++
@@ -1,8 +0,0 @@
-function figure_itgExpOpt_CloseRequestFcn(obj, evd, h_fig)
-h = guidata(h_fig);
-if isfield(h, 'itgExpOpt');
-    h = rmfield(h, {'itgExpOpt', 'figure_itgExpOpt'});
-    guidata(h_fig, h);
-end
-
-delete(obj);
