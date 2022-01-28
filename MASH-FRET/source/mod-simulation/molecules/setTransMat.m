a7b9cc6d94eb42bf513697f1995ee2cebc06df6c
mca-sh <melodiehadzic@protonmail.com>
2022-01-28 10:00:58 +0100

    correct transition rates display in simulation

--- MASH-FRET/source/mod-simulation/molecules/setTransMat.m
+++ MASH-FRET/source/mod-simulation/molecules/setTransMat.m
@@ -21,11 +21,11 @@ else
     str_un = 'frame-1';
 end
 
-h_ed = [h.edit11 h.edit21 h.edit31 h.edit41 h.edit51 
-    h.edit12 h.edit22 h.edit32 h.edit42 h.edit52
-    h.edit13 h.edit23 h.edit33 h.edit43 h.edit53
-    h.edit14 h.edit24 h.edit34 h.edit44 h.edit54
-    h.edit15 h.edit25 h.edit35 h.edit45 h.edit55];
+h_ed = [h.edit11,h.edit12,h.edit13,h.edit14,h.edit15;...
+    h.edit21,h.edit22,h.edit23,h.edit24,h.edit25;...
+    h.edit31,h.edit32,h.edit33,h.edit34,h.edit35;...
+    h.edit41,h.edit42,h.edit43,h.edit44,h.edit45;...
+    h.edit51,h.edit52,h.edit53,h.edit54,h.edit55];
 
 J = size(h_ed,1);
 
@@ -33,9 +33,9 @@ for j1 = 1:J
     for j2 = 1:J
         ttstr = wrapHtmlTooltipString(sprintf(['transition rate constant ',...
             '<b>%i->%i</b> (in ',str_un,')'],j1,j2));
-        set(h_ed(j1,j2),'string',num2str(transMat(j2,j1)),'tooltipstring',...
+        set(h_ed(j1,j2),'string',num2str(transMat(j1,j2)),'tooltipstring',...
             ttstr);
-        if transMat(j2,j1)==0
+        if transMat(j1,j2)==0
             set(h_ed(j1,j2),'backgroundcolor',lightgray);
         end
     end
