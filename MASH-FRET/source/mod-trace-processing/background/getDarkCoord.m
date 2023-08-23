ce2f4f09d0b07699ed35da4d7a1fea9d914247c8
mca-sh <melodiehadzic@protonmail.com>
2023-08-23 16:23:53 +0200

    correct automatic determination of dark coordinates by taking into account the size of the integration area

--- MASH-FRET/source/mod-trace-processing/background/getDarkCoord.m
+++ MASH-FRET/source/mod-trace-processing/background/getDarkCoord.m
@@ -1,15 +1,25 @@
-function dark_coord = getDarkCoord(exc, mol, c, p, dimImg)
+function dark_coord = getDarkCoord(exc, mol, c, proj, dimImg)
+% dark_coord = getDarkCoord(exc, mol, c, proj, dimImg)
+%
+% Determines the pixel position of the darkest intensity in the molecule's 
+% average sub-image.
+%
+% exc: laser's chronological order (integer number between 1 and laser nb.)
+% mol: molecule's index in sample
+% c: video channel index
+% proj: structure containing project's data and parameters
+% dimImg: pixel dimensions of the square sub-image
 
-proj = p.curr_proj;
+% collect project's data
+viddim = proj.movie_dim;
+coord = proj.coord(mol,(2*c-1):2*c);
+nChan = proj.nb_channel;
+aDim = proj.pix_intgr(1);
 
-viddim = p.proj{proj}.movie_dim;
-coord = p.proj{proj}.coord(mol,(2*c-1):2*c);
-nChan = p.proj{proj}.nb_channel;
-aDim = p.proj{proj}.pix_intgr(1);
+% determine channel-specific pixel limits
 multichanvid = numel(viddim)==1;
-
 if multichanvid
-    aveImg = p.proj{proj}.aveImg{exc+1};
+    aveImg = proj.aveImg{exc+1};
     res_x = viddim{1}(1);
     res_y = viddim{1}(2);
     split = round(res_x/nChan)*(1:nChan-1);
@@ -17,37 +27,31 @@ if multichanvid
     lim_x = [split_x(c) split_x(c+1)];
     lim_y = [0 res_y];
 else
-    aveImg = p.proj{proj}.aveImg{c,exc+1};
+    aveImg = proj.aveImg{c,exc+1};
     res_x = viddim{c}(1);
     res_y = viddim{c}(2);
     lim_x = [0 res_x];
     lim_y = [0 res_y];
 end
 
+% determines sub-image pixel limits
 lim = getSubimgLim(coord, dimImg, [lim_x;lim_y]);
-lim_img_x = lim(1,:);
-lim_img_y = lim(2,:);
+lim_img_x = lim(1,:)+1;
+lim_img_y = lim(2,:)+1;
 
-lim_img_x(1) = lim_img_x(1) + 1;
-range_x = ceil(lim_img_x(1):lim_img_x(2));
-lim_img_y(1) = lim_img_y(1) + 1;
-range_y = ceil(lim_img_y(1):lim_img_y(2));
+% lists all possibly integratable pixel coordinates within the sub-image
+pixX = ceil(lim_img_x(1):lim_img_x(2));
+pixY = ceil(lim_img_y(1):lim_img_y(2));
+[X,Y] = meshgrid(pixX,pixY);
+pixCoord = [reshape(X,numel(X),1),reshape(Y,numel(Y),1)];
+incl = pixCoord(:,2)>=(aDim/2) & pixCoord(:,2)<=(res_y-aDim/2) &...
+    pixCoord(:,1)>=aDim & pixCoord(:,1)<=(res_x-aDim/2);
+pixCoord = pixCoord(incl,:);
 
-dark_y = 0;
-dark_x = 0;
-[X,Y] = meshgrid(range_x,range_y);
-coord_img = [reshape(X,1,numel(X));reshape(Y,1,numel(Y))];
-
-while (dark_y < aDim/2 || dark_y > (res_y-aDim/2)|| dark_x < aDim || ...
-        dark_x > (res_x-aDim/2))
-    [o,ind] = sort(reshape(aveImg(range_y,range_x),1,numel(range_y)* ...
-        numel(range_x)));
-    ind = ind(round(numel(ind)/2));
-    aveImg(coord_img(2,ind),coord_img(1,ind)) = Inf;
-
-    dark_y = coord_img(2,ind) - 0.5;
-    dark_x = coord_img(1,ind) - 0.5;
-end
-
-dark_coord = [dark_x dark_y];
+% identifies the darkest pixel intensity within the sub-image
+pixSum = summolpix(aveImg,pixCoord,aDim);
+[~,ind] = sort(pixSum);
+% ind = ind(round(numel(ind)/2)); % median
+ind = ind(1); % minimum
+dark_coord = [pixCoord(ind,1),pixCoord(ind,2)]-0.5;
 
