66df4e62dbbefac0c51c1608a121fa688ef3163f
mca-sh <melodiehadzic@protonmail.com>
2023-10-17 22:30:08 +0200

    correct update of BOBA checkbox value in HA

--- MASH-FRET/source/mod-histogram-analysis/_callbacks/checkbox_thm_BS_Callback.m
+++ MASH-FRET/source/mod-histogram-analysis/_callbacks/checkbox_thm_BS_Callback.m
@@ -9,9 +9,9 @@ proj = p.curr_proj;
 tpe = p.thm.curr_tpe(proj);
 tag = p.thm.curr_tag(proj);
 
-p.proj{proj}.HA.prm{tag,tpe}.thm_start{1}(2) = get(obj, 'Value');
-p.proj{proj}.HA.prm{tag,tpe}.thm_res(1,1:3) = {[] [] []};
-p.proj{proj}.HA.prm{tag,tpe}.thm_res(2,1:3) = {[] [] []};
+p.proj{proj}.HA.curr{tag,tpe}.thm_start{1}(2) = get(obj, 'Value');
+p.proj{proj}.HA.curr{tag,tpe}.thm_res(1,1:3) = {[] [] []};
+p.proj{proj}.HA.curr{tag,tpe}.thm_res(2,1:3) = {[] [] []};
     
 h.param = p;
 guidata(h_fig, h);
