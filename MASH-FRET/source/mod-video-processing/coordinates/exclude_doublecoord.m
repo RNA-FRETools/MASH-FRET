f4f3fd94d2ff9ab4f8c968f8f91d8027550cb8b3
mca-sh <melodiehadzic@protonmail.com>
2024-01-08 12:05:37 +0100

    accelerate detection of double coordinates

--- MASH-FRET/source/mod-video-processing/coordinates/exclude_doublecoord.m
+++ MASH-FRET/source/mod-video-processing/coordinates/exclude_doublecoord.m
@@ -8,17 +8,25 @@ function ok = exclude_doublecoord(tol, coord)
 
 % Last update: 10th of February 2014 by Mélodie C.A.S Hadzic
 
-[n,o] = size(coord);
-ok = true(n,1);
+[N,~] = size(coord);
+ok = true(N,1);
+id = (1:N)';
 
 for j = 1:size(coord,1)-1
-    if ok(j,1)
-        for i = j+1:size(coord,1)
-            if sum((abs(coord(j,1:2:end)-coord(i,1:2:end))<tol)) && ...
-                    sum((abs(coord(j,2:2:end)-coord(i,2:2:end))<tol))
-                ok(i,1) = false;
-                ok(j,1) = true;
-            end
-        end
+    if ~ok(j,1)
+        continue
     end
+    isdbl = id~=j & abs(coord(j,1:2:end)-coord(:,1:2:end))<tol & ...
+        abs(coord(j,2:2:end)-coord(:,2:2:end))<tol;
+    if any(isdbl)
+        ok(j,1) = false;
+        ok(isdbl',1) = false;
+    end
+%     for i = j+1:size(coord,1)
+%         if sum((abs(coord(j,1:2:end)-coord(i,1:2:end))<tol)) && ...
+%                 sum((abs(coord(j,2:2:end)-coord(i,2:2:end))<tol))
+%             ok(i,1) = false;
+%             ok(j,1) = true;
+%         end
+%     end
 end
