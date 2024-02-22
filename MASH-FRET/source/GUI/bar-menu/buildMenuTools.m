e38fe831639019e1171ff7edea4d1b78178c83a8
mca-sh <melodiehadzic@protonmail.com>
2024-02-22 12:49:04 +0100

    fix bugs discovered in menu bar test routine

--- MASH-FRET/source/GUI/bar-menu/buildMenuTools.m
+++ MASH-FRET/source/GUI/bar-menu/buildMenuTools.m
@@ -4,21 +4,17 @@ function h = buildMenuTools(h)
 % h = buildMenuTools(h);
 %
 % h: structure to update with handles to new UI components and that must contain fields:
-% h.menu_tools: handle to menu "Overwrite files"
+% h.menu_tools: handle to menu "Tools"
 
 % created by MH, 07.01.2019
 
 % default
-lbl0 = 'Restructure files';
-lbl2 = 'Check toolbox dependencies';
+lbl0 = 'Check toolbox dependencies';
 
 % parents
 h_men = h.menu_tools;
 
 % GUI
-h.menu_restructFiles = uimenu(h_men,'label',lbl0,'callback',...
-    @menu_restructFiles_Callback);
-
-h.menu_toolboxDependency = uimenu(h_men,'label',lbl2);
+h.menu_toolboxDependency = uimenu(h_men,'label',lbl0);
 h = buildMenuToolboxDependency(h);
 
