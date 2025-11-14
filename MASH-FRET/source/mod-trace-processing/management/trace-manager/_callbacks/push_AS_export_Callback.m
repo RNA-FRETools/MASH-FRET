5e43da253f96377d051d7be3586ce0305f3171f3
Mélodie Hadzic <melodiehadzic@protonmail.com>
2025-11-14 15:03:05 +0100

    add info about which data is histogrammed in auto-sort data dump file and add suffix _autosort to file name

--- MASH-FRET/source/mod-trace-processing/management/trace-manager/_callbacks/push_AS_export_Callback.m
+++ MASH-FRET/source/mod-trace-processing/management/trace-manager/_callbacks/push_AS_export_Callback.m
@@ -4,9 +4,25 @@ h = guidata(h_fig);
 p = h.param;
 proj = p.curr_proj;
 projnm = p.proj{proj}.exp_parameters{1,2};
-[fle,loc] = uiputfile('.txt','Enter file name',[projnm,'.txt']);
+[fle,loc] = uiputfile('.txt','Enter file name',projnm);
+if isempty(fle) || (isscalar(fle) && fle==0)
+    return
+end
 [~,flenm,~] = fileparts(fle);
-fle = [loc,flenm,'.txt'];
+if strcmp(flenm(end-length('_autosort')+1:end),'_autosort')
+    flenm = flenm(1:end-length('_autosort'));
+end
+fle = [loc,flenm,'_autosort.txt'];
+               
+% collect data names
+xdat = removeHtml(...
+    h.tm.popupmenu_selectXdata.String{h.tm.popupmenu_selectXdata.Value});
+xval = removeHtml(...
+    h.tm.popupmenu_selectXval.String{h.tm.popupmenu_selectXval.Value});
+ydat = removeHtml(...
+    h.tm.popupmenu_selectYdata.String{h.tm.popupmenu_selectYdata.Value});
+yval = removeHtml(...
+    h.tm.popupmenu_selectYval.String{h.tm.popupmenu_selectYval.Value});
 
 for chld = h.tm.axes_histSort.Children
     for c = 1:numel(chld)
@@ -23,6 +39,7 @@ for chld = h.tm.axes_histSort.Children
                 fdat = [edg',[x,NaN]',[cnt,NaN]'];
 
                 f = fopen(fle,'Wt');
+                fprintf(f,'data: %s, %s\n',xdat,xval);
                 fprintf(f,fhead);
                 fprintf(f,'%d\t%d\t%d\n',fdat');
                 fclose(f);
@@ -36,7 +53,14 @@ for chld = h.tm.axes_histSort.Children
                 y = mean([yedg(1:end-1);yedg(2:end)],1);
                 cnt = chld(c).BinCounts; % [nbinx-by-nbiny]
                 fdat = [[NaN;y'],[x;cnt']];
-                save(fle,'fdat','-ascii');
+                fmt = repmat('%d\t',1,size(fdat,2));
+                fmt(end) = 'n';
+
+                f = fopen(fle,'Wt');
+                fprintf(f,'x-data: %s, %s\n',xdat,xval);
+                fprintf(f,'y-data: %s, %s\n',ydat,yval);
+                fprintf(f,fmt,fdat');
+                fclose(f);
     
                 break
     
